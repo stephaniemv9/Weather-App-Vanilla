137389cf39ccae60e17de66f8e6dc8caba877d33
Stephanie <stephaniemvillamar@gmail.com>
2022-08-23 15:04:16 -0400

    Updated the time, date
    Updated the time, date

--- src/app.js
+++ src/app.js
@@ -1,5 +1,24 @@
 function formatDate(timestamp) {
-  return "Friday 5:00";
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
@@ -15,7 +34,7 @@ function displayTemperature(response) {
   weatherDescription.innerHTML = response.data.weather[0].description;
   humidtyElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = "";
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "75a21154277faea77d1df0f6f6cc4662";
